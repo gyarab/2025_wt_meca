224bf1d16125ffe554b6082598aab63e46abd360
Viktor Meca <viktor.meca.s@gyarab.cz>
2026-02-03 15:00:07 +0100

    Fix HTTP request URL by removing session ID

--- kurzy/exchange.py
+++ kurzy/exchange.py
@@ -1,6 +1,6 @@
 import httpx
 
-r = httpx.get('https://www.cnb.cz/cs/financni-trhy/devizovy-trh/kurzy-devizoveho-trhu/kurzy-devizoveho-trhu/denni_kurz.txt;jsessionid=CE90DB9B266D3B51A3856C0794DE7D42')
+r = httpx.get('https://www.cnb.cz/cs/financni-trhy/devizovy-trh/kurzy-devizoveho-trhu/kurzy-devizoveho-trhu/denni_kurz.txt')
 print(r.text)
 print(r.status)
 lines = r.text.split('\n')
